e97bbb42ebbdb14fe1a2df8c08a70cebf0755005
YellowDuckcument <109415258+YellowDuckcument@users.noreply.github.com>
2023-02-17 11:44:24 +0700

    modified:   src/components/MiniBasket.js 	modified:   src/pages/ShopBasket.js

--- src/pages/ShopBasket.js
+++ src/pages/ShopBasket.js
@@ -1,11 +1,23 @@
+import { Nav } from "react-bootstrap";
 import TopBanner from "../components/TopBanner";
 
 const ShopBasket = () => {
-    return ( 
-        <>
-            <TopBanner  content='Thông tin đơn hàng'/>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <TopBanner content="Thông tin đơn hàng" />
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+};
+
 export default ShopBasket;
