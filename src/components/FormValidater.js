2d3a06a52d43aa02df3f49a9884159904a240f60
YellowDuckcument <109415258+YellowDuckcument@users.noreply.github.com>
2023-02-24 16:04:55 +0700

    test

---
+++ src/components/FormValidater.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Nav } from "react-bootstrap";
+import FormInfo from "./FormInfo";
+import FormLogin from "./FormLogin";
+
+function FormValidater() {
+    const [numberKey, setNumberKey] = useState('');
+
+  return (
+    <>
+      <Nav
+        justify
+        variant="tabs"
+        defaultActiveKey="/home"
+        className="formValidater d-flex justify-content-center"
+      >
+        <Nav.Item>
+          <Nav.Link className="rounded-0" href="#" onClick={() => setNumberKey('0')}>
+            Địa chỉ giao hàng
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="rounded-0" eventKey="link-1" onClick={() => setNumberKey('1')}>
+            Đăng nhập
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div className="d-flex justify-content-center bg-light" style={{height:"511px"}}>
+        {numberKey === '0' ? <FormInfo /> : <FormLogin /> }
+      </div>
+    </>
+  );
+}
+
+export default FormValidater;
--- src/pages/ShopBasket.js
+++ src/pages/ShopBasket.js
@@ -1,22 +1,61 @@
-import { Nav } from "react-bootstrap";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import FormValidater from "../components/FormValidater";
 import TopBanner from "../components/TopBanner";
 
 const ShopBasket = () => {
   return (
-    <>
+    <div className="bg-secondary p-4">
       <TopBanner content="Thông tin đơn hàng" />
-      <Nav defaultActiveKey="/home" as="ul">
-        <Nav.Item as="li">
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </>
+      <Container className="bg-light my-4" fluid>
+        <Container>
+          <Nav className="justify-content-evenly" defaultActiveKey="/" as="ul">
+            <Nav.Item as="li">
+              <Nav.Link className="d-flex" href="#">
+                <div>
+                  <div className="square d-flex justify-content-center align-items-center bg-primary rounded-circle">
+                    <div className="text text-light fs-6">1</div>
+                  </div>
+                </div>
+                <div className="text align-self-center ms-2 text-uppercase fw-semibold">
+                  Thông tin đơn hàng
+                </div>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link className="d-flex" href="#">
+                <div>
+                  <div className="square d-flex justify-content-center align-items-center border border-primary rounded-circle">
+                    <div className="text text-primary fs-6">2</div>
+                  </div>
+                </div>
+                <div className="text align-self-center ms-2 text-uppercase fw-semibold">
+                  Hình thức thanh toán
+                </div>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link className="d-flex" href="#">
+                <div className="square d-flex justify-content-center align-items-center border border-primary rounded-circle">
+                  <div className="text text-primary fs-6">3</div>
+                </div>
+                <div className="text align-self-center ms-2 text-uppercase fw-semibold">
+                  Hoàn thành
+                </div>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col sm="8">
+            <FormValidater />
+          </Col>
+          <Col sm="4">1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
